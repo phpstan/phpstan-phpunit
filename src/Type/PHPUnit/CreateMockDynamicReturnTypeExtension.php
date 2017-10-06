17ad5dc75fbfd510459bec64d04114bb94b9d141
Ondrej Mirtes <ondrej@mirtes.cz>
2017-10-06 04:55:52 +0200

    Support getMockFromWsdl

--- src/Type/PHPUnit/CreateMockDynamicReturnTypeExtension.php
+++ src/Type/PHPUnit/CreateMockDynamicReturnTypeExtension.php
@@ -12,6 +12,13 @@ use PHPStan\Type\TypeCombinator;
 class CreateMockDynamicReturnTypeExtension implements \PHPStan\Type\DynamicMethodReturnTypeExtension
 {
 
+	/** @var int[] */
+	private $methods = [
+		'createMock' => 0,
+		'getMockForAbstractClass' => 0,
+		'getMockFromWsdl' => 1,
+	];
+
 	public static function getClass(): string
 	{
 		return \PHPUnit\Framework\TestCase::class;
@@ -19,22 +26,16 @@ class CreateMockDynamicReturnTypeExtension implements \PHPStan\Type\DynamicMetho
 
 	public function isMethodSupported(MethodReflection $methodReflection): bool
 	{
-		return in_array(
-			$methodReflection->getName(),
-			[
-				'createMock',
-				'getMockForAbstractClass',
-			],
-			true
-		);
+		return array_key_exists($methodReflection->getName(), $this->methods);
 	}
 
 	public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
 	{
-		if (count($methodCall->args) === 0) {
+		$argumentIndex = $this->methods[$methodReflection->getName()];
+		if (!isset($methodCall->args[$argumentIndex])) {
 			return $methodReflection->getReturnType();
 		}
-		$arg = $methodCall->args[0]->value;
+		$arg = $methodCall->args[$argumentIndex]->value;
 		if (!($arg instanceof \PhpParser\Node\Expr\ClassConstFetch)) {
 			return $methodReflection->getReturnType();
 		}

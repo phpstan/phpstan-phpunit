669990e486c1efe7d4a2f6d337c229f6a317795b
Martin Hujer <mhujer@gmail.com>
2017-12-21 16:45:47 +0100

    AssertSameWithCountRule

---
+++ src/Rules/PHPUnit/AssertSameWithCountRule.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\PHPUnit;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+
+class AssertSameWithCountRule implements \PHPStan\Rules\Rule
+{
+
+	public function getNodeType(): string
+	{
+		return \PhpParser\NodeAbstract::class;
+	}
+
+	/**
+	 * @param \PhpParser\Node\Expr\MethodCall|\PhpParser\Node\Expr\StaticCall $node
+	 * @param \PHPStan\Analyser\Scope $scope
+	 * @return string[] errors
+	 */
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if (!AssertRuleHelper::isMethodOrStaticCallOnTestCase($node, $scope)) {
+			return [];
+		}
+
+		if (count($node->args) < 2) {
+			return [];
+		}
+		if (!is_string($node->name) || strtolower($node->name) !== 'assertsame') {
+			return [];
+		}
+
+		$right = $node->args[1]->value;
+
+		if (
+			$right instanceof Node\Expr\FuncCall
+			&& $right->name instanceof Node\Name
+			&& strtolower($right->name->toString()) === 'count'
+		) {
+			return [
+				'You should use assertCount($expectedCount, $variable) instead of assertSame($expectedCount, count($variable)).',
+			];
+		}
+
+		return [];
+	}
+
+}

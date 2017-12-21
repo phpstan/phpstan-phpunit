29f95535e851229dcaf5be8ccc05db9825c120e3
Martin Hujer <mhujer@gmail.com>
2017-12-21 16:17:46 +0100

    AssertSameNullExpectedRule

---
+++ tests/Rules/PHPUnit/AssertSameNullExpectedRuleTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\PHPUnit;
+
+use PHPStan\Rules\Rule;
+
+class AssertSameNullExpectedRuleTest extends \PHPStan\Testing\RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return new AssertSameNullExpectedRule();
+	}
+
+	public function testRule()
+	{
+		$this->analyse([__DIR__ . '/data/assert-same-null-expected.php'], [
+			[
+				'You should use assertNull() instead of assertSame(null, $actual).',
+				10,
+			],
+			[
+				'You should use assertNull() instead of assertSame(null, $actual).',
+				13,
+			],
+		]);
+	}
+
+}
---
+++ tests/Rules/PHPUnit/data/assert-same-null-expected.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace ExampleTestCase;
+
+class AssertSameNullExpectedTestCase extends \PHPUnit\Framework\TestCase
+{
+
+	public function testAssertSameWithNullAsExpected()
+	{
+		$this->assertSame(null, 'a');
+
+		$a = null;
+		$this->assertSame($a, 'b');
+
+		$this->assertSame('a', 'b'); // OK
+
+		/** @var string|null $c */
+		$c = null;
+		$this->assertSame($c, 'foo'); // nullable is OK
+	}
+
+}

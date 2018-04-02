5115c2bfb80b22e8dd7ef9d0c64e8583b92c08f9
Ondrej Mirtes <ondrej@mirtes.cz>
2018-04-02 12:47:24 +0200

    Updated type descriptions in test for detecting wrong asserts

--- tests/Rules/PHPUnit/AssertSameMethodDifferentTypesRuleTest.php
+++ tests/Rules/PHPUnit/AssertSameMethodDifferentTypesRuleTest.php
@@ -28,55 +28,55 @@ class AssertSameMethodDifferentTypesRuleTest extends \PHPStan\Testing\RuleTestCa
 	{
 		$this->analyse([__DIR__ . '/data/assert-same.php'], [
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with string and int(1) will always evaluate to false.',
 				10,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with string and stdClass will always evaluate to false.',
 				11,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with int(1) and string will always evaluate to false.',
 				12,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with string and int will always evaluate to false.',
 				13,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with array<int(0)|int(1), string> and array<int(0)|int(1), int(1)|int(2)> will always evaluate to false.',
 				14,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with array<string> and array<int> will always evaluate to false.',
 				39,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to true.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with int(1) and int(1) will always evaluate to true.',
 				44,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with array<int(0), string> and array<int(0)|int(1), string> will always evaluate to false.',
 				45,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to true.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with string and string will always evaluate to true.',
 				46,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with string and string will always evaluate to false.',
 				47,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to true.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with stdClass and stdClass will always evaluate to true.',
 				48,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with array<int(0), string> and array<int(0)|int(1), int(1)|string> will always evaluate to false.',
 				51,
 			],
 			[
-				'Call to method PHPUnit\Framework\Assert::assertSame() will always evaluate to false.',
+				'Call to method PHPUnit\Framework\Assert::assertSame() with array<int(0)|int(1)|int(2), float(3.000000)|int(2)|string> and array<int(0)|int(1), int(1)|string> will always evaluate to false.',
 				52,
 			],
 		]);

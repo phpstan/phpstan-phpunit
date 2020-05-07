eb5d83c74ed7355a88314bad88b8bbcad6f83ddb
Vincent Langlet <vincentlanglet@hotmail.fr>
2020-05-07 21:39:50 +0200

    Report error when trying to configure a non existing method on MockObject

---
+++ tests/Rules/PHPUnit/MockMethodCallRuleTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\PHPUnit;
+
+use PHPStan\Rules\Rule;
+
+/**
+ * @extends \PHPStan\Testing\RuleTestCase<MockMethodCallRule>
+ */
+class MockMethodCallRuleTest extends \PHPStan\Testing\RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return new MockMethodCallRule();
+	}
+
+	public function testRule(): void
+	{
+		$this->analyse([__DIR__ . '/data/mock-method-call.php'], [
+			[
+				'Trying to mock an undefined method doBadThing() on class MockMethodCall\Bar.',
+				15,
+			],
+			[
+				'Trying to mock an undefined method doBadThing() on class MockMethodCall\Bar.',
+				20,
+			],
+		]);
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [
+			__DIR__ . '/../../../extension.neon',
+		];
+	}
+
+}
---
+++ tests/Rules/PHPUnit/data/mock-method-call.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types = 1);
+
+namespace MockMethodCall;
+
+class Foo extends \PHPUnit\Framework\TestCase
+{
+
+	public function testGoodMethod()
+	{
+		$this->createMock(Bar::class)->method('doThing');
+	}
+
+	public function testBadMethod()
+	{
+		$this->createMock(Bar::class)->method('doBadThing');
+	}
+
+	public function testBadMethodWithExpectation()
+	{
+		$this->createMock(Bar::class)->expects($this->once())->method('doBadThing');
+	}
+
+	public function testWithAnotherObject()
+	{
+		$bar = new BarWithMethod();
+		$bar->method('doBadThing');
+	}
+
+}
+
+class Bar {
+	public function doThing()
+	{
+		return 1;
+	}
+};
+
+class BarWithMethod {
+	public function method(string $string)
+	{
+		return $string;
+	}
+};

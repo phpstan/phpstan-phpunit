1cbc8ea73c2c25a5fef4bc335b8a982055093ff9
Simon <simonhammes.27@gmail.com>
2021-07-14 12:42:31 +0200

    Add support for assertArrayHasKey (#100)

--- tests/Type/PHPUnit/data/assert-function.php
+++ tests/Type/PHPUnit/data/assert-function.php
@@ -3,6 +3,8 @@
 namespace AssertFunction;
 
 use function PHPStan\Testing\assertType;
+use function PHPUnit\Framework\assertArrayHasKey;
+use function PHPUnit\Framework\assertArrayNotHasKey;
 use function PHPUnit\Framework\assertInstanceOf;
 
 class Foo
@@ -15,6 +17,17 @@ class Foo
 	{
 		assertInstanceOf(self::class, $o);
 		assertType(self::class, $o);
+	}
+
+	public function arrayHasNumericKey(array $a): void {
+		assertArrayHasKey(0, $a);
+		assertType('array&hasOffset(0)', $a);
+	}
+
+	public function arrayHasStringKey(array $a): void
+	{
+		assertArrayHasKey('key', $a);
+		assertType("array&hasOffset('key')", $a);
 	}
 
 }

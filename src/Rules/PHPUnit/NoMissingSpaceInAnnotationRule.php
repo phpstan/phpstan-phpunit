c3de23b677a95b009a30f83596aacd5d02f0d1b8
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-05-04 21:06:03 +0200

    Add rule that checks for invalid and unrecognized annotations

---
+++ src/Rules/PHPUnit/NoMissingSpaceInAnnotationRule.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPStan\Rules\PHPUnit;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @implements Rule<Node>
+ */
+class NoMissingSpaceInAnnotationRule implements Rule
+{
+	private const ANNOTATIONS_WITH_PARAMS = [
+		'backupGlobals',
+		'backupStaticAttributes',
+		'covers',
+		'coversDefaultClass',
+		'dataProvider',
+		'depends',
+		'group',
+		'preserveGlobalState',
+		'requires',
+		'testDox',
+		'testWith',
+		'ticket',
+		'uses'
+	];
+
+	public function getNodeType(): string
+	{
+		return Node::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if (!$node instanceof Node\Stmt\Class_ && !$node instanceof Node\Stmt\ClassMethod) {
+			return [];
+		}
+
+		if ($node instanceof Node\Stmt\Class_) {
+			if (is_subclass_of($node->namespacedName->toString(), TestCase::class) === false) {
+				return [];
+			}
+		} else {
+			$classReflection = $scope->getClassReflection();
+			if ($classReflection === null || $classReflection->isSubclassOf(TestCase::class) === false) {
+				return [];
+			}
+		}
+
+		$docComment = $node->getDocComment();
+		if ($docComment === null) {
+			return [];
+		}
+
+		$errors = [];
+		foreach (preg_split("/((\r?\n)|(\r\n?))/", $docComment->getText()) as $docCommentLine) {
+			// These annotations can't be retrieved using the getResolvedPhpDoc method on the FileTypeMapper as they are not present when they are invalid
+			$annotation = preg_match('/@(?<property>[a-zA-Z]+)(?<whitespace>\s*)(?<value>.*)/', $docCommentLine, $matches);
+			if ($annotation === false) {
+				continue; // Line without annotation
+			}
+
+			if (in_array($matches['property'], self::ANNOTATIONS_WITH_PARAMS, true) && $matches['whitespace'] === '') {
+				$errors[] = 'Annotation "' . $matches[0] . '" is invalid, "@' . $matches['property'] . '" should be followed by a space and a value.';
+			}
+		}
+
+		return $errors;
+	}
+}
---
+++ tests/Rules/PHPUnit/NoMissingSpaceInAnnotationRuleTest.php
@@ -0,0 +1,140 @@
+<?php
+declare(strict_types=1);
+
+namespace Rules\PHPUnit;
+
+use PHPStan\Rules\PHPUnit\NoMissingSpaceInAnnotationRule;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<NoMissingSpaceInAnnotationRule>
+ */
+class NoMissingSpaceInAnnotationRuleTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return new NoMissingSpaceInAnnotationRule();
+	}
+
+	public function testRule(): void
+	{
+		$this->analyse([__DIR__ . '/data/InvalidCoversAnnotation.php'], [
+			[
+				'Annotation "@backupGlobals" is invalid, "@backupGlobals" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@backupStaticAttributes" is invalid, "@backupStaticAttributes" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@covers\Dummy\Foo::assertSame" is invalid, "@covers" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@covers::assertSame" is invalid, "@covers" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@coversDefaultClass\Dummy\Foo" is invalid, "@coversDefaultClass" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@dataProvider" is invalid, "@dataProvider" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@depends" is invalid, "@depends" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@preserveGlobalState" is invalid, "@preserveGlobalState" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@requires" is invalid, "@requires" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@testDox" is invalid, "@testDox" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@testWith" is invalid, "@testWith" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@ticket" is invalid, "@ticket" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@uses" is invalid, "@uses" should be followed by a space and a value.',
+				36,
+			],
+			[
+				'Annotation "@backupGlobals" is invalid, "@backupGlobals" should be followed by a space and a value.',
+				43,
+			],
+			[
+				'Annotation "@backupStaticAttributes" is invalid, "@backupStaticAttributes" should be followed by a space and a value.',
+				50,
+			],
+			[
+				'Annotation "@covers\Dummy\Foo::assertSame" is invalid, "@covers" should be followed by a space and a value.',
+				58,
+			],
+			[
+				'Annotation "@covers::assertSame" is invalid, "@covers" should be followed by a space and a value.',
+				58,
+			],
+			[
+				'Annotation "@coversDefaultClass\Dummy\Foo" is invalid, "@coversDefaultClass" should be followed by a space and a value.',
+				64,
+			],
+			[
+				'Annotation "@dataProvider" is invalid, "@dataProvider" should be followed by a space and a value.',
+				70,
+			],
+			[
+				'Annotation "@depends" is invalid, "@depends" should be followed by a space and a value.',
+				76,
+			],
+			[
+				'Annotation "@preserveGlobalState" is invalid, "@preserveGlobalState" should be followed by a space and a value.',
+				83,
+			],
+			[
+				'Annotation "@requires" is invalid, "@requires" should be followed by a space and a value.',
+				89,
+			],
+			[
+				'Annotation "@testDox" is invalid, "@testDox" should be followed by a space and a value.',
+				95,
+			],
+			[
+				'Annotation "@testWith" is invalid, "@testWith" should be followed by a space and a value.',
+				101,
+			],
+			[
+				'Annotation "@ticket" is invalid, "@ticket" should be followed by a space and a value.',
+				107,
+			],
+			[
+				'Annotation "@uses" is invalid, "@uses" should be followed by a space and a value.',
+				113,
+			]
+		]);
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [
+			__DIR__ . '/../../../extension.neon',
+		];
+	}
+}

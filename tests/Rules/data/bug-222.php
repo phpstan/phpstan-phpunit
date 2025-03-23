f511925a3b811eac7512dbd152ad65258123cb6d
Vincent Langlet <vincentlanglet@hotmail.fr>
2025-03-23 18:33:48 +0100

    Add non regression test

---
+++ tests/Rules/data/bug-222.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Rules\data;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class Foo extends TestCase
+{
+
+	public function doFoo(MockObject $mockService): void
+	{
+		$mockService
+			->expects($this->exactly(1))
+			->method('get')
+			->with(24)
+			->willReturn('24');
+	}
+
+}

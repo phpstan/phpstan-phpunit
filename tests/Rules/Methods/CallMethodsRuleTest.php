1a07095989e956832e1fd0f36eb422c1ab8ca725
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2025-03-23 23:01:32 +0100

    Add non regression test for #222

---
+++ tests/Rules/Methods/CallMethodsRuleTest.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Methods;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<CallMethodsRule>
+ */
+class CallMethodsRuleTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return self::getContainer()->getByType(CallMethodsRule::class);
+	}
+
+	public function testBug222(): void
+	{
+		$this->analyse([__DIR__ . '/data/bug-222.php'], []);
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [
+			__DIR__ . '/../../../extension.neon',
+		];
+	}
+
+}
---
+++ tests/Rules/Methods/data/bug-222.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bug222;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class Foo extends TestCase
+{
+
+	public function doFoo(MockObject $mockService): void
+	{
+		$mockService
+			->expects($this->exactly(1))
+			->method('get')
+			->with(24)
+			->willReturn('24');
+
+		$mockService
+			->method('get')
+			->with(24)
+			->willReturn('24');
+
+		$mockService
+			->expects($this->exactly(1))
+			->method('get')
+			->willReturn('24');
+
+		$mockService
+			->method('get')
+			->willReturn('24');
+	}
+
+}

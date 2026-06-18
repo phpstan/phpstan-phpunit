a0035477ecb85eb266dc19f3e14af48705797151
Markus Staab <markus.staab@redaxo.de>
2026-06-18 17:46:03 +0200

    Warn about incomplete versions in `#[RequiresPhp]`

--- src/Rules/PHPUnit/AttributeRequiresPhpVersionRule.php
+++ src/Rules/PHPUnit/AttributeRequiresPhpVersionRule.php
@@ -36,21 +36,28 @@ class AttributeRequiresPhpVersionRule implements Rule
 	private PhpVersion $fallbackPhpVersion;
 
 	/**
-	 * When phpstan-deprecation-rules is installed, it reports deprecated usages.
+	 * When phpstan-deprecation-rules is installed, rule reports deprecated usages.
 	 */
 	private bool $deprecationRulesInstalled;
+
+	/**
+	 * Whether warnings about incomplete versions are allowed to be emitted
+	 */
+	private bool $warnAboutIncompleteVersion;
 
 	public function __construct(
 		PHPUnitVersion $PHPUnitVersion,
 		TestMethodsHelper $testMethodsHelper,
 		bool $deprecationRulesInstalled,
-		PhpVersion $phpVersion
+		PhpVersion $phpVersion,
+		bool $warnAboutIncompleteVersion = true
 	)
 	{
 		$this->PHPUnitVersion = $PHPUnitVersion;
 		$this->testMethodsHelper = $testMethodsHelper;
 		$this->deprecationRulesInstalled = $deprecationRulesInstalled;
 		$this->fallbackPhpVersion = $phpVersion;
+		$this->warnAboutIncompleteVersion = $warnAboutIncompleteVersion;
 	}
 
 	public function getNodeType(): string
@@ -87,6 +94,15 @@ class AttributeRequiresPhpVersionRule implements Rule
 			// see https://github.com/sebastianbergmann/phpunit/blob/43c2cd7b96ee1e800b35e4df23b419a88b53111d/src/Metadata/Version/Requirement.php
 
 			$versionRequirement = $args[0];
+
+			if ($this->warnAboutIncompleteVersion($versionRequirement)) {
+				$errors[] = RuleErrorBuilder::message(
+					sprintf('Version requirement is incomplete.'),
+				)
+					->identifier('phpunit.incompletePhpVersion')
+					->build();
+			}
+
 			if (
 				!is_numeric($versionRequirement)
 			) {
@@ -178,6 +194,20 @@ class AttributeRequiresPhpVersionRule implements Rule
 		}
 
 		return [new Version($this->fallbackPhpVersion->getVersionString())];
+	}
+
+	// see https://github.com/sebastianbergmann/phpunit/issues/6451
+	function warnAboutIncompleteVersion(string $versionRequirement): bool
+	{
+		if (!$this->warnAboutIncompleteVersion) {
+			return false;
+		}
+
+		if (!$this->PHPUnitVersion->warnsAboutIncompleteVersion()->yes()) {
+			return false;
+		}
+
+		return substr_count($versionRequirement, '.') !== 2;
 	}
 
 }
